bd08e97f55062e33c7e262ec8615bdc03663c95f
Bruno Barros <bruno.barros@zup.com.br>
2019-12-21 18:20:30 -0300

    implementando log com campos passando via MDC

---
+++ src/main/java/com/example/log4j2demo/CompareValue.java
@@ -0,0 +1,38 @@
+package com.example.log4j2demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.MDC;
+
+public class CompareValue {
+    private static final Logger logger = LogManager.getLogger(CompareValue.class);
+    private final int valueA;
+    private final int valueB;
+
+    public CompareValue(int valueA, int valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+    }
+
+    public void execute()
+    {
+        ThreadContext.put("valorA", String.valueOf(valueA));
+        MDC.put("valorA","MDC " + String.valueOf(valueA));
+        MDC.put("valorB", "MDC " + String.valueOf(valueB));
+        logger.info("[CompareValue] - Começando a comparar os valores {}, {}", valueA, valueB);
+        try{
+            if (valueA == valueB)
+            {
+                logger.info("[CompareValue] - Os valores são iguais");
+            }else
+            {
+                logger.warn("[CompareValue] - Valores são diferentes");
+            }
+
+        }catch (Exception e)
+        {
+            logger.error("[CompareValeu] - Ocorreu esse problema: {}", e);
+        }
+    }
+}
--- src/main/java/com/example/log4j2demo/Log4j2DemoApplication.java
+++ src/main/java/com/example/log4j2demo/Log4j2DemoApplication.java
@@ -18,11 +18,8 @@ public class Log4j2DemoApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
-        logger.debug("Debugging log");
-        logger.info("Info log");
-        logger.warn("Hey, This is a warning!");
-        logger.error("Oops! We have an Error. OK");
-        logger.fatal("Damn! Fatal error. Please fix me.");
+        CompareValue compareValue = new CompareValue(2, 2);
+        compareValue.execute();
     }
 
 }
